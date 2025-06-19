f8d74b87400ab6dc0ec6dee6b91949f4751f9b54
kdobrovolsky <kdobrovolsky02@gmail.com>
2025-06-19 19:36:03 +0300

    hw3

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -26,9 +26,9 @@ export const pureOnBlur = (name: string, setError: (error: string) => void) => {
  }
 }
 
-export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: ()=>void) => { // если нажата кнопка Enter - добавить
+export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: ()=>void ) => { // если нажата кнопка Enter - добавить
     if(e.key === 'Enter'){
-        e.preventDefault()
+       
         addUser()
     }
 }
