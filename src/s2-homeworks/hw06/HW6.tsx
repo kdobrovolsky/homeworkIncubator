6857bf031f1dfbe90f2f205a80926f9a998d0b04
kdobrovolsky <kdobrovolsky02@gmail.com>
2025-08-10 18:25:27 +0300

    test

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,7 +18,8 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        setValue(restoreState<string>('editable-span-value',''))
+        const restoredValue = restoreState<string>('hw6-editable-span-value', '')
+        setValue(restoredValue)
     }
 
     return (
